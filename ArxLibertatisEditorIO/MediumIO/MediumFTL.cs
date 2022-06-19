894330abe1794a62c3977a4e43ca7ad9d7efcb3c
Frederik Gelder <frederik.gelder@freenet.de>
2022-06-19 13:13:28 +0200

    add classes to make save and load of FTL easier

---
+++ ArxLibertatisEditorIO/MediumIO/MediumFTL.cs
@@ -0,0 +1,34 @@
+﻿using ArxLibertatisEditorIO.MediumIO.FTL;
+using ArxLibertatisEditorIO.RawIO;
+using ArxLibertatisEditorIO.RawIO.FTL;
+
+namespace ArxLibertatisEditorIO.MediumIO
+{
+    public static class MediumFTL
+    {
+        public static Ftl LoadModel(FTL_IO rawFtl)
+        {
+            Ftl ftl = new Ftl();
+            ftl.LoadFrom(rawFtl);
+            return ftl;
+        }
+
+        public static Ftl LoadModel(string path)
+        {
+            var rawFtl = RawFTL.LoadModel(path);
+            return LoadModel(rawFtl);
+        }
+
+        public static void SaveModel(Ftl ftl, FTL_IO rawFtl)
+        {
+            ftl.WriteTo(rawFtl);
+        }
+
+        public static void SaveModel(Ftl ftl, string path)
+        {
+            var rawFtl = new FTL_IO();
+            ftl.WriteTo(rawFtl);
+            RawFTL.SaveModel(rawFtl, path);
+        }
+    }
+}
---
+++ ArxLibertatisEditorIO/RawIO/RawFTL.cs
@@ -0,0 +1,37 @@
+﻿using ArxLibertatisEditorIO.RawIO.FTL;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ArxLibertatisEditorIO.RawIO
+{
+    public static class RawFTL
+    {
+        public static FTL_IO LoadModel(string path)
+        {
+            FTL_IO ftl = new FTL_IO();
+
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                var s = FTL_IO.EnsureUnpacked(fs);
+                ftl.ReadFrom(s);
+            }
+            return ftl;
+        }
+
+        public static void SaveModel(FTL_IO ftl, string path)
+        {
+            using (var ms = new MemoryStream())
+            {
+                ftl.WriteTo(ms);
+                var packed = FTL_IO.EnsurePacked(ms);
+                using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
+                {
+                    packed.CopyTo(fs);
+                }
+                packed.Dispose();
+            }
+        }
+    }
+}
