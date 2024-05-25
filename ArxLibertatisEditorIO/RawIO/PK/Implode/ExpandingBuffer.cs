1b253d8af5bea994f9906ab59f50238b5d098fea
Frederik Gelder <frederik.gelder@freenet.de>
2024-05-26 00:00:21 +0200

    try copying lalis implementation of pkware compression

---
+++ ArxLibertatisEditorIO/RawIO/PK/Implode/ExpandingBuffer.cs
@@ -0,0 +1,278 @@
+﻿using ArxLibertatisEditorIO.MediumIO.Shared;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ArxLibertatisEditorIO.RawIO.PK.Implode
+{
+    public class ExpandingBuffer
+    {
+        const int blockSize = 0x1000;
+
+        byte[] heap;
+        int startIndex = 0, startIndexBackup = 0;
+        int endIndex = 0, endIndexBackup = 0;
+
+        public ExpandingBuffer(int numberOfBytes = 0)
+        {
+            heap = new byte[numberOfBytes];
+        }
+
+        ReadOnlySpan<byte> getActualDataAsSpan(int offset = 0)
+        {
+            var start = startIndex + offset;
+            return heap.AsSpan(start, endIndex - start);
+        }
+
+        ReadOnlyMemory<byte> getActualDataAsMemory(int offset = 0)
+        {
+            var start = startIndex + offset;
+            return heap.AsMemory(start, endIndex - start);
+        }
+
+        /// <summary>
+        /// Returns the number of bytes in the stored data.
+        /// </summary>
+        /// <returns></returns>
+        public int size()
+        {
+            return endIndex - startIndex;
+        }
+
+        public bool isEmpty()
+        {
+            return size() == 0;
+        }
+
+        /// <summary>
+        /// Returns the underlying Buffer's (heap) size.
+        /// </summary>
+        /// <returns></returns>
+        public int heapSize()
+        {
+            return heap.Length;
+        }
+
+        /// <summary>
+        /// Sets a single byte of the stored data
+        /// If offset is negative, then the method calculates the index from the end backwards
+        /// </summary>
+        /// <param name="offset"></param>
+        /// <param name="value"></param>
+        public void setByte(int offset, byte value)
+        {
+            if (offset < 0)
+            {
+                if (endIndex + offset < startIndex)
+                {
+                    return;
+                }
+
+                heap[endIndex + offset] = value;
+                return;
+            }
+            if (startIndex + offset >= endIndex) //TODO: this looks wrong, check with lali
+            {
+                heap[startIndex + offset] = value;
+            }
+        }
+
+        /// <summary>
+        /// Adds a single byte to the end of the stored data.
+        /// This expands the internal buffer by 0x1000 bytes if the heap is full
+        /// </summary>
+        /// <param name="value"></param>
+        public void appendByte(byte value)
+        {
+            if (endIndex + 1 < heapSize())
+            {
+                heap[endIndex] = value;
+                endIndex += 1;
+                return;
+            }
+
+            var currentData = getActualDataAsSpan();
+
+            heap = new byte[((int)Math.Ceiling((currentData.Length + 1) / (double)blockSize) + 1) * blockSize];
+            currentData.CopyTo(heap);
+            heap[currentData.Length] = value;
+            startIndex = 0;
+            endIndex = currentData.Length + 1;
+        }
+
+        /// <summary>
+        /// Concatenates a buffer to the end of the stored data.
+        /// If the new data exceeds the size of the heap then the internal heap
+        /// gets expanded by the integer multiples of 0x1000 bytes
+        /// </summary>
+        /// <param name="newData"></param>
+        public void append(ReadOnlySpan<byte> newData)
+        {
+            if (this.endIndex + newData.Length < this.heapSize())
+            {
+                newData.CopyTo(this.heap.AsSpan().Slice(this.endIndex));
+                this.endIndex += newData.Length;
+                return;
+            }
+
+            var currentData = this.getActualDataAsSpan();
+
+            this.heap = new byte[
+              ((int)Math.Ceiling((currentData.Length + newData.Length) / (double)blockSize) + 1) * blockSize
+            ];
+            currentData.CopyTo(this.heap);
+            newData.CopyTo(this.heap.AsSpan().Slice(currentData.Length));
+
+            this.startIndex = 0;
+            this.endIndex = currentData.Length + newData.Length;
+        }
+
+        /// <summary>
+        /// Returns a slice of data from the internal data.
+        /// offset and a limit can be specified:
+        /// offset determines the starting position, limit specifies the number of bytes read.
+        ///
+        /// Watch out! The returned slice of Buffer points to the same Buffer in memory!
+        /// This is intentional for performance reasons.
+        /// </summary>
+        /// <param name="offset"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        public ReadOnlyMemory<byte> read(int offset, int limit)
+        {
+            if (offset < 0 || limit < 1)
+            {
+                return ReadOnlyMemory<byte>.Empty;
+            }
+
+            if (offset + limit < this.size())
+            {
+                return this.heap.AsMemory(this.startIndex + offset, limit);
+            }
+
+            return this.getActualDataAsMemory(offset);
+        }
+
+
+        /// <summary>
+        /// Reads a single byte from the stored data
+        /// </summary>
+        /// <param name="offset"></param>
+        /// <returns></returns>
+        public byte readByte(int offset = 0)
+        {
+            return this.heap[this.startIndex + offset];
+        }
+
+        /// <summary>
+        /// Does hard delete
+        ///
+        /// Removes data from the start of the internal buffer(heap)
+        /// by copying bytes to lower indices making sure the
+        /// startIndex goes back to 0 afterwards
+        /// </summary>
+        /// <param name="numberOfBytes"></param>
+        public void flushStart(int numberOfBytes)
+        {
+            numberOfBytes = Math.Clamp(numberOfBytes, 0, this.heapSize());
+            if (numberOfBytes > 0)
+            {
+                if (numberOfBytes < this.heapSize())
+                {
+                    this.heap.AsSpan(startIndex + numberOfBytes).CopyTo(this.heap);
+                }
+
+                this.endIndex -= this.startIndex + numberOfBytes;
+                this.startIndex = 0;
+            }
+        }
+
+        /// <summary>
+        /// Does hard delete
+        /// Removes data from the end of the internal buffer (heap)
+        /// by moving the endIndex back
+        /// </summary>
+        /// <param name="numberOfBytes"></param>
+        public void flushEnd(int numberOfBytes)
+        {
+            var clampedNumberOfBytes = Math.Clamp(numberOfBytes, 0, this.heapSize());
+            if (clampedNumberOfBytes > 0)
+            {
+                this.endIndex -= clampedNumberOfBytes;
+            }
+        }
+
+        /// <summary>
+        /// Does soft delete
+        ///
+        /// Removes data from the start of the internal buffer(heap)
+        /// by moving the startIndex forward
+        /// When the heap gets empty it also resets the indices as a cleanup
+        /// </summary>
+        /// <param name="numberOfBytes"></param>
+        public void dropStart(int numberOfBytes)
+        {
+            if (numberOfBytes <= 0)
+            {
+                return;
+            }
+
+            this.startIndex += numberOfBytes;
+            if (this.startIndex >= this.endIndex)
+            {
+                this.clear();
+            }
+        }
+        /// <summary>
+        /// Does soft delete
+        ///
+        /// removes data from the end of the internal buffer(heap)
+        /// by moving the endIndex back
+        /// When the heap gets empty it also resets the indices as a cleanup
+        /// </summary>
+        /// <param name="numberOfBytes"></param>
+        public void dropEnd(int numberOfBytes)
+        {
+            if (numberOfBytes <= 0)
+            {
+                return;
+            }
+
+            this.endIndex -= numberOfBytes;
+            if (this.startIndex >= this.endIndex)
+            {
+                this.clear();
+            }
+        }
+
+        /// <summary>
+        /// returns the internal buffer
+        /// </summary>
+        /// <returns></returns>
+        public byte[] getHeap()
+        {
+            return this.heap;
+        }
+
+        public void clear()
+        {
+            this.startIndex = 0;
+            this.endIndex = 0;
+        }
+
+        public void saveIndices()
+        {
+            this.startIndexBackup = this.startIndex;
+            this.endIndexBackup = this.endIndex;
+        }
+
+        public void restoreIndices()
+        {
+            this.startIndex = this.startIndexBackup;
+            this.endIndex = this.endIndexBackup;
+        }
+    }
+}
---
+++ ArxLibertatisEditorIO/RawIO/PK/Implode/ImplodeFromLali.cs
@@ -0,0 +1,80 @@
+﻿using ArxLibertatisEditorIO.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+
+namespace ArxLibertatisEditorIO.RawIO.PK.Implode
+{
+    //from https://github.com/arx-tools/node-pkware/blob/5e75119e5571972c5524231bf10c91a78a09ea44/src/bin/implode.ts
+
+    public class ImplodeFromLali
+    {
+        bool verbose = false;
+        bool isFirstChunk = true;
+        ExpandingBuffer inputBuffer;
+        ExpandingBuffer outputBuffer;
+        int chunkCounter = 0;
+        Compression compressionType = Compression.Unknown;
+        DictionarySize dictionarySize = DictionarySize.Unknown;
+        int dictionarySizeMask = -1;
+        bool streamEnded = false;
+        byte[] distCodes = (byte[])Constants.DistCode.Clone();
+        byte[] distBits = (byte[])Constants.DistBits.Clone();
+        int startIndex = 0;
+        bool handledFirstTwoBytes = false;
+        int outBits = 0;
+        byte[] nChBits = new byte[0x306];
+        byte[] nChCodes = new byte[0x306];
+
+        public ImplodeFromLali(Compression compressionType, DictionarySize dictionarySize, int inputBufferSize = 0, int outputBufferSize = 0, bool verbose = false)
+        {
+            this.compressionType = compressionType;
+            this.dictionarySize = dictionarySize;
+            this.inputBuffer = new ExpandingBuffer(inputBufferSize);
+            this.outputBuffer = new ExpandingBuffer(outputBufferSize);
+            this.verbose = verbose;
+        }
+
+        //TODO: omg lali what is this function??
+        //public object getHandler()
+        //{
+        //return (object/*Transform*/ dis, byte[] chunk, object /*BufferEncoding*/ encoding, object callback) =>
+        //{
+
+        //};
+        //}
+
+
+        public static byte[] DoImplode(byte[] bytes)
+        {
+            MemoryStream output = new MemoryStream();
+
+            var header = new ImplodeHeader();
+
+            header.literalSize = ImplodeLiteralSize.Fixed;
+            header.dictSize = ImplodeDictSize.Size1024;
+
+            using (var sw = new StructWriter(output, Encoding.UTF8, true))
+            {
+                sw.WriteStruct(header);
+            }
+
+            BitStream bits = new BitStream();
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bits.WriteFixedLiteral(bytes[i]);
+            }
+            bits.WriteEOS();
+            while (bits.ByteReady())
+            {
+                output.WriteByte(bits.GetByte());
+            }
+            output.WriteByte(bits.GetBytePadded());
+
+            return output.ToArray();
+        }
+    }
+}
